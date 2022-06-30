ebd879eb5008139365c6975073a9d077e293a6a1
hanjunpeng <1358271059@qq.com>
2022-06-30 11:22:22 +0800

    IFullScreenLayout提交代码

--- src/components/IFullScreenLayout.vue
+++ src/components/IFullScreenLayout.vue
@@ -163,7 +163,7 @@ export default {
       chooseGridMediaList:[],
       //当前适配确定后的响应式布局
       productionMediaGridList:[],
-      computedWidth: {width: 'calc(100vw)'}
+      computedWidth: {}
     }
   },
   props: {
@@ -202,6 +202,7 @@ export default {
       });
       ro.observe(document.querySelector("#"+this.moduleObject.id));
     });
+    this.convertAttrCompontedWidth();
   },
   destroyed() {},
   methods:{
@@ -1125,10 +1126,23 @@ export default {
      */
     converAttrToTable () {
       this.chooseGridMediaList = this.propData.chooseGridMediaList;
-      console.log(this.moduleObject, '获取moduleObject');
-      // if (this.moduleObject.env === 'develop') {
-
-      // }
+    },
+    convertAttrCompontedWidth () {
+      this.sendBroadcastMessage({ type: 'getMenuWidth', globalSend: true });
+    },
+    /**
+     * 组件通信：发送消息的方法
+     * @param {
+     *  type:"自己定义的，统一规定的type：linkageResult（组件联动传结果值）、linkageDemand（组件联动传需求值）、linkageReload（联动组件重新加载）
+     * 、linkageOpenDialog（打开弹窗）、linkageCloseDialog（关闭弹窗）、linkageShowModule（显示组件）、linkageHideModule（隐藏组件）、linkageResetDefaultValue（重置默认值）",
+     *  message:{实际的消息对象},
+     *  rangeModule:"为空发送给全部，根据配置的属性中设定的值（值的内容是组件的packageid数组），不取子表的，比如直接 this.$root.propData.compositeAttr["attrKey"]（注意attrKey是属性中定义的bindKey）,这里的格式为：['1','2']"",
+     *  className:"指定的组件类型，比如只给待办组件发送，然后再去过滤上面的值"
+     *  globalSend:如果为true则全站发送消息，注意全站rangeModule是无效的，只有className才有效，默认为false
+     * } object
+     */
+    sendBroadcastMessage(object) {
+        window.IDM.broadcast && window.IDM.broadcast.send(object);
     },
     /**
      * 组件通信：接收消息的方法
@@ -1141,11 +1155,12 @@ export default {
      * } object
      */
     receiveBroadcastMessage (object) {
-      console.log(object, '全屏布局获取消息');
+      console.log(123232, object)
       switch (object.type) {
-        // 刷新菜单收缩 menuCollapse true：收缩 false：展开
-        case 'changeMenuCollapse':
-          this.computedWidth = (object.message || {}).menuCollapse ? {width: `calc(100vw - ${this.propData.widthSmall})`} : {width: `calc(100vw - ${this.propData.widthBig})`};
+        // 刷新菜单收缩 menuWidthChange从菜单发起 true：收缩 false：展开
+        case 'menuWidthChange':
+          console.log(object, '全屏布局获取消息');
+          this.computedWidth = this.moduleObject.env === 'develop' ? {width: `calc(${(IDM.develop.getDragWorkspaceInfo() || {}).width} - ${object.message.menuWidth})`} : {width: `calc(100vw - ${object.message.menuWidth})`};
           break;
       }
     },
